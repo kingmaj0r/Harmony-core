d45b64f9243b9202f453dae344ad4e73e63c2012
KingMaj0r <abdurrahman@barakat.email>
2024-05-20 10:45:10 +0700

    modifed bump_version script

--- .github/scripts/bump_version.sh
+++ .github/scripts/bump_version.sh
@@ -7,18 +7,7 @@ if [ -z "$latest_release" ]; then
   major=$(echo $version | cut -d. -f1)
   minor=$(echo $version | cut -d. -f2)
   patch=$(echo $version | cut -d. -f3)
-  NEW_VERSION="$major.$minor.$((patch + 1))"
+  new_version="$major.$minor.$((patch + 1))"
 else
-  IFS='.' read -ra version_parts <<< "$version"
-  IFS='.' read -ra latest_release_parts <<< "$latest_release"
-
-  major=${version_parts[0]}
-  minor=${version_parts[1]}
-  patch=${version_parts[2]}
-
-  if [ "${latest_release_parts[0]}" -eq "$major" ] && [ "${latest_release_parts[1]}" -eq "$minor" ]; then
-    NEW_VERSION="$major.$minor.$((patch + 1))"
-  else
-    NEW_VERSION="$major.$((minor + 1)).0"
-  fi
+  new_version=$(node -e "console.log('$version'.split('.').map((v,i) => i===2?parseFloat(v)+0.1:parseInt(v)).join('.'))")
 fi
