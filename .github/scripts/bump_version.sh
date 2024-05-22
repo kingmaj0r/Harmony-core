73a6806453cce809f92b0c6d7ab9304bf3276b94
KingMaj0r <abdurrahman@barakat.email>
2024-05-22 10:01:12 +0700

    UPDATE bump_version.sh

--- .github/scripts/bump_version.sh
+++ .github/scripts/bump_version.sh
@@ -4,50 +4,10 @@ version=$(node -p "require('./package.json').version")
 latest_release=$(gh release list --limit 1 --json tagName --jq '.[0].tag_name')
 
 if [ -z "$latest_release" ]; then
-  IFS='.' read -ra parts <<< "$version"
-  major=${parts[0]}
-  minor=${parts[1]}
-  patch=${parts[2]}
-  ((patch++))
-
-  if (( patch > 99 )); then
-    patch=0
-    ((minor++))
-
-    if (( minor > 9 )); then
-      minor=0
-      ((major++))
-    fi
-  fi
-
-  new_version="$major.$minor.$patch"
+  major=$(echo $version | cut -d. -f1)
+  minor=$(echo $version | cut -d. -f2)
+  patch=$(echo $version | cut -d. -f3)
+  new_version="$major.$minor.$((patch + 1))"
 else
-  IFS='V' read -r _ latest_version <<< "$latest_release"
-  IFS='.' read -ra latest_parts <<< "$latest_version"
-
-  major=${parts[0]}
-  minor=${parts[1]}
-  patch=${parts[2]}
-
-  major_latest=${latest_parts[0]}
-  minor_latest=${latest_parts[1]}
-  patch_latest=${latest_parts[2]}
-
-  if (( major > major_latest )); then
-    major=$((major + 1))
-    minor=0
-    patch=0
-  elif (( major == major_latest && minor > minor_latest )); then
-    minor=$((minor + 1))
-    patch=0
-  elif (( major == major_latest && minor == minor_latest && patch >= patch_latest )); then
-    echo "Error: Version $version is equal to or lower than the latest release ($latest_release)."
-    exit 1
-  else
-    ((patch++))
-  fi
-
-  new_version="$major.$minor.$patch"
+  new_version=$(node -e "let versionParts = '$version'.split('.'); let major = versionParts[0]; let minor = versionParts[1]; let patch = versionParts[2]; let updatedPatch = parseFloat(patch); if (patch === '99') { updatedPatch = 0; minor = parseInt(minor) + 1; } else { updatedPatch += 0.1; } console.log([major, minor, updatedPatch].join('.'));")
 fi
-
-echo $new_version
